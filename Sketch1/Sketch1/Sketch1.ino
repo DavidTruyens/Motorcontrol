7a7e829cc38b834622b3da136e86a49b52aea62b
David Truyens <david.truyens@gmail.com>
2018-02-04 14:06:46 +0100

    Constant speed

--- Sketch1/Sketch1/Sketch1.ino
+++ Sketch1/Sketch1/Sketch1.ino
@@ -2,9 +2,9 @@
 DualVNH5019MotorShield md;
 
 /* Encoder Library - Basic Example
-* http://www.pjrc.com/teensy/td_libs_Encoder.html
-*
-* This example code is in the public domain.
+  http://www.pjrc.com/teensy/td_libs_Encoder.html
+
+  This example code is in the public domain.
 */
 
 #include "Encoder.h"
@@ -22,18 +22,18 @@ Encoder myEnc(3, 5);
 double Setpoint, Input, Output;
 
 //Specify the links and initial tuning parameters
-PID myPID(&Input, &Output, &Setpoint, 0.05, 0.1, 0.0001, DIRECT);
+PID myPID(&Input, &Output, &Setpoint, 1, 0.5, 0.001, DIRECT);
 
 
 void setup()
 {
-	Serial.begin(9600);
-	Serial.println("Dual VNH5019 Motor Shield");
-	md.init();
-	Setpoint = 1000;
-	//turn the PID on
-	myPID.SetOutputLimits(-2000, 2000);
-	myPID.SetMode(AUTOMATIC);
+  Serial.begin(9600);
+  Serial.println("Dual VNH5019 Motor Shield");
+  md.init();
+  Setpoint = 40;
+  //turn the PID on
+  myPID.SetOutputLimits(0, 400);
+  myPID.SetMode(AUTOMATIC);
 
 }
 
@@ -42,32 +42,34 @@ float Pvalue = 1;
 float gain = 0;
 unsigned long prevtime = 0;
 unsigned long interval = 1000;
-double speedincr = 10;
+//double speedincr = 10;
 
 void loop()
 {
-	encoder();
-	Input = speed;
-	myPID.Compute();
-	md.setM1Speed(int(Output));
-	stopIfFault;
-	if (millis() - prevtime >= interval) {
-		prevtime = millis();
+  encoder();
+  Input = speed;
+  myPID.Compute();
+  md.setM1Speed(int(Output));
+  stopIfFault;
 
-		if (Setpoint >= 2000) {
-			speedincr = -50;
-		}
-		else if (Setpoint <= -2000) {
-			speedincr = 50;
-		}
-
-		Setpoint = Setpoint + speedincr;
-
-		Serial.print("Setpoint= ");
-		Serial.println(Setpoint);
-	}
-
+  //  if (millis() - prevtime >= interval) {
+  //    prevtime = millis();
+  //
+  //    if (Setpoint >= 2000) {
+  //      speedincr = -50;
+  //    }
+  //    else if (Setpoint <= -2000) {
+  //      speedincr = 50;
+  //    }
+  //
+  //    Setpoint = Setpoint; //+ speedincr;
+  //  if (millis() - prevtime >= interval) {
+  //    Serial.print("Setpoint= ");
+  //    Serial.println(Setpoint);
+  //  }
 }
+
+
 
 long oldPosition = -999;
 long prevpos = 0;
@@ -77,32 +79,35 @@ long lasttime;
 long deltat;
 
 void encoder() {
-	long newPosition = myEnc.read();
-	if (newPosition != oldPosition) {
-		oldPosition = newPosition;
-	}
-	deltat = millis() - lasttime;
+  long newPosition = myEnc.read();
+  if (newPosition != oldPosition) {
+    oldPosition = newPosition;
+  }
+  deltat = millis() - lasttime;
 
-	if (deltat >= 10) {
-		lasttime = millis();
-		deltap = newPosition - prevpos;
-		speed = deltap * 1000 / deltat;
-		Serial.print("speed: ");
-		Serial.println(speed);
-		prevpos = newPosition;
-	}
+  if (deltat >= 100) {
+    lasttime = millis();
+    deltap = newPosition - prevpos;
+    speed = deltap * 1000 / deltat;
+    if (millis() - prevtime >= interval) {
+      prevtime = millis();
+      Serial.print("speed: ");
+      Serial.println(speed);
+    }
+    prevpos = newPosition;
+  }
 }
 
 void stopIfFault()
 {
-	if (md.getM1Fault())
-	{
-		Serial.println("M1 fault");
-		while (1);
-	}
-	if (md.getM2Fault())
-	{
-		Serial.println("M2 fault");
-		while (1);
-	}
+  if (md.getM1Fault())
+  {
+    Serial.println("M1 fault");
+    while (1);
+  }
+  if (md.getM2Fault())
+  {
+    Serial.println("M2 fault");
+    while (1);
+  }
 }
